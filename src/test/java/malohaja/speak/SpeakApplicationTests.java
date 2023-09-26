c5494c0018becb3a652d67de9768bbc8bd68d3f5
ktk8916 <ktk8916@naver.com>
2023-09-26 22:01:43 +0900

    init

---
+++ src/test/java/malohaja/speak/SpeakApplicationTests.java
@@ -0,0 +1,13 @@
+package malohaja.speak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
