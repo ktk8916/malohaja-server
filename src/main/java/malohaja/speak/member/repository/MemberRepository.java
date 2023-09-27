7ac5a888ae531ceed892e29d433164d6a716ed61
ktk8916 <ktk8916@naver.com>
2023-09-28 04:35:14 +0900

    oauth2 login 추가

--- src/main/java/malohaja/speak/member/repository/MemberRepository.java
+++ src/main/java/malohaja/speak/member/repository/MemberRepository.java
@@ -3,5 +3,14 @@ package malohaja.speak.member.repository;
 import malohaja.speak.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    /*
+    select * from member m
+    where m.provider_name = ?
+    and m.provider_id = ?
+     */
+    Optional<Member> findByProviderNameAndProviderId(String providerName, String providerId);
 }
