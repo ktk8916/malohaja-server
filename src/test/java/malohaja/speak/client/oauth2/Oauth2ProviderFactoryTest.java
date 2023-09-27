36dc6f034aa557134439ef67e76191b8f7c3c312
ktk8916 <ktk8916@naver.com>
2023-09-28 04:36:49 +0900

    띄어쓰기 추가

--- src/test/java/malohaja/speak/client/oauth2/Oauth2ProviderFactoryTest.java
+++ src/test/java/malohaja/speak/client/oauth2/Oauth2ProviderFactoryTest.java
@@ -23,6 +23,17 @@ class Oauth2ProviderFactoryTest {
     @Autowired
     private Oauth2ProviderFactory oauth2ProviderFactory;
 
+    @DisplayName("유효하지 않은 providerName인 경우, 예외를 발생시킨다.")
+    @Test
+    void inValidOauth2ProviderName(){
+        // given
+        String inValidOauth2ProviderName = "그냥누가봐도틀린이름임";
+
+        // when, then
+        assertThatThrownBy(()->oauth2ProviderFactory.getByProviderName(inValidOauth2ProviderName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     private static Stream<Arguments> oauth2ProviderTokenUri(){
         return Stream.of(
                 Arguments.of("github", "https://github.com/login/oauth/access_token", "https://api.github.com/user"),
@@ -30,25 +41,17 @@ class Oauth2ProviderFactoryTest {
         );
     }
 
-    @DisplayName("유효하지 않은 providerName인 경우, 예외를 발생시킨다.")
-    @Test
-    void inValidOauth2ProviderName(){
-        //given
-        String inValidOauth2ProviderName = "그냥누가봐도틀린이름임";
-
-        //when, then
-        assertThatThrownBy(()->oauth2ProviderFactory.getByProviderName(inValidOauth2ProviderName))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
     @DisplayName("providerName으로 생성된 Oauth2Provider를 가져온다.")
     @ParameterizedTest
     @MethodSource("oauth2ProviderTokenUri")
     void getByProviderName(String providerName, String tokenUri, String userInfoUri){
-        //given
+        // given
+        // oauth2ProviderTokenUri()
+
+        // when
         Oauth2Provider provider = oauth2ProviderFactory.getByProviderName(providerName);
 
-        //when, then
+        // then
         assertThat(provider.getTokenUri()).isEqualTo(tokenUri);
         assertThat(provider.getUserInfoUri()).isEqualTo(userInfoUri);
     }
