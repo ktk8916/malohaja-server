026db2b21901fe86acbdbc9aba3cd86c83683a7c
ktk8916 <ktk8916@naver.com>
2023-09-28 03:06:52 +0900

    getUserResource 추가

---
+++ src/main/java/malohaja/speak/client/oauth2/domain/Oauth2TokenDto.java
@@ -0,0 +1,12 @@
+package malohaja.speak.client.oauth2.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record Oauth2TokenDto(
+        @JsonProperty("token_type")
+        String tokenType,
+        @JsonProperty("access_token")
+        String accessToken,
+        String scope
+) {
+}
---
+++ src/main/java/malohaja/speak/client/oauth2/domain/Oauth2UserInfoDto.java
@@ -0,0 +1,7 @@
+package malohaja.speak.client.oauth2.domain;
+
+public record Oauth2UserInfoDto(
+        String providerName,
+        String providerId
+) {
+}
