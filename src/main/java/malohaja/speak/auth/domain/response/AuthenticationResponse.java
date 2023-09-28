992fa3364a69a7b5e10cdfdc10b874a0151c3efc
ktk8916 <ktk8916@naver.com>
2023-09-28 14:17:07 +0900

    refresh token 제거.. 후에 추가 해야함

--- src/main/java/malohaja/speak/auth/domain/response/AuthenticationResponse.java
+++ src/main/java/malohaja/speak/auth/domain/response/AuthenticationResponse.java
@@ -1,15 +1,17 @@
 package malohaja.speak.auth.domain.response;
 
+/*
+우선 access token만 사용하도록 빠르게 구현하고
+후에 refresh token을 도입하자
+ */
 public record AuthenticationResponse(
         String redirect,
-        String accessToken,
-        String refreshToken
+        String accessToken
 ) {
-    public static AuthenticationResponse of(String redirect, String accessToken, String refreshToken){
+    public static AuthenticationResponse of(String redirect, String accessToken){
         return new AuthenticationResponse(
                 redirect,
-                accessToken,
-                refreshToken
+                accessToken
         );
     }
 }
