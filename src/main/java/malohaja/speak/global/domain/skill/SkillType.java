cca40693c3c5547630ef40fd54039846f5e5d0f5
ktk8916 <ktk8916@naver.com>
2023-09-30 23:37:27 +0900

    string to skill 로직 추가

--- src/main/java/malohaja/speak/global/domain/skill/SkillType.java
+++ src/main/java/malohaja/speak/global/domain/skill/SkillType.java
@@ -2,6 +2,12 @@ package malohaja.speak.global.domain.skill;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import malohaja.speak.global.exception.NoMatchingSkillException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @RequiredArgsConstructor
@@ -13,4 +19,17 @@ public enum SkillType {
     GIT(BelongType.ETC);
 
     private final BelongType belongType;
+
+    public static SkillType fromString(String skill){
+        return Arrays.stream(SkillType.values())
+                .filter(skillType -> skillType.name().equalsIgnoreCase(skill))
+                .findFirst()
+                .orElseThrow(NoMatchingSkillException::new);
+    }
+
+    public static Set<SkillType> convertList(List<String> skills){
+        return skills.stream()
+                .map(SkillType::fromString)
+                .collect(Collectors.toSet());
+    }
 }
