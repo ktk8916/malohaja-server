718863a11c14bfcec83b8681d5205909d7dadaac
ktk8916 <ktk8916@naver.com>
2023-09-30 18:11:00 +0900

    jwt 토큰 생성, 추출 테스트

--- src/test/java/malohaja/speak/global/jwt/JwtServiceTest.java
+++ src/test/java/malohaja/speak/global/jwt/JwtServiceTest.java
@@ -1,18 +1,47 @@
 package malohaja.speak.global.jwt;
 
+import malohaja.speak.member.entity.Member;
+import malohaja.speak.member.entity.Role;
+import malohaja.speak.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("dev")
+@Transactional
 class JwtServiceTest {
 
     @Autowired
     private JwtService jwtService;
 
+    @Autowired
+    private MemberRepository memberRepository;
 
+    @DisplayName("member로 jwt 토큰을 생성하고, 토큰에서 member를 추출한다.")
+    @Test
+    void generateToken(){
+        // given
+        Member member = Member.builder()
+                .providerName("kakao")
+                .providerId("kakaoId")
+                .role(Role.MEMBER)
+                .build();
 
+        Member savedMember = memberRepository.save(member);
+        String token = jwtService.generateToken(member);
+
+        // when
+        TokenInfo tokenInfo = jwtService.extractUser(token);
+
+        // then
+        Assertions.assertThat(tokenInfo.getId()).isEqualTo(1L);
+        Assertions.assertThat(tokenInfo.getProviderName()).isEqualTo("kakao");
+        Assertions.assertThat(tokenInfo.getProviderId()).isEqualTo("kakaoId");
+        Assertions.assertThat(tokenInfo.getRole()).isEqualTo(Role.MEMBER);
+    }
 }
