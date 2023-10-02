4939bd1492bb7dce08a14f4be6a01d101b8184a2
ktk8916 <ktk8916@naver.com>
2023-10-02 14:51:38 +0900

    예외 추가

---
+++ src/main/java/malohaja/speak/interview/question/exception/InvalidQuestionWriterException.java
@@ -0,0 +1,6 @@
+package malohaja.speak.interview.question.exception;
+
+import malohaja.speak.global.exception.MalohajaException;
+
+public class InvalidQuestionWriterException extends MalohajaException {
+}
---
+++ src/main/java/malohaja/speak/member/exception/MemberNotFoundException.java
@@ -0,0 +1,6 @@
+package malohaja.speak.member.exception;
+
+import malohaja.speak.global.exception.MalohajaException;
+
+public class MemberNotFoundException extends MalohajaException {
+}
