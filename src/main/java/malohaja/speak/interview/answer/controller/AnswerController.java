dbab7367182a46671086ef0887d83c7d0c95e50f
ktk8916 <ktk8916@naver.com>
2023-10-04 00:54:20 +0900

    파일 업로드 테스트 나중에 지워야함

--- src/main/java/malohaja/speak/interview/answer/controller/AnswerController.java
+++ src/main/java/malohaja/speak/interview/answer/controller/AnswerController.java
@@ -1,11 +1,19 @@
 package malohaja.speak.interview.answer.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import malohaja.speak.interview.answer.service.AnswerService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/answer")
 public class AnswerController {
+
+    private final AnswerService answerService;
+
+    @PostMapping
+    public String test(@RequestParam("file") MultipartFile file){
+        return answerService.register(file);
+    }
 }
