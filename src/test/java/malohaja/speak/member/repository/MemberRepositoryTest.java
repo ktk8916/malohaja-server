aa172e919232b71fb65373d9a17c028910a4c4d5
ktk8916 <ktk8916@naver.com>
2023-10-04 00:15:57 +0900

    닉네임 조회 테스트

--- src/test/java/malohaja/speak/member/repository/MemberRepositoryTest.java
+++ src/test/java/malohaja/speak/member/repository/MemberRepositoryTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
 @DataJpaTest
 class MemberRepositoryTest {
 
@@ -31,7 +35,30 @@ class MemberRepositoryTest {
         Member findMember = memberRepository.findByProviderNameAndProviderId(providerName, providerId).get();
 
         // then
-        Assertions.assertThat(findMember.getProviderName()).isEqualTo(providerName);
-        Assertions.assertThat(findMember.getProviderId()).isEqualTo(providerId);
+        assertThat(findMember.getProviderName()).isEqualTo(providerName);
+        assertThat(findMember.getProviderId()).isEqualTo(providerId);
+    }
+
+    @DisplayName("nickname으로 member를 조회한다.")
+    @Test
+    void findByNickname(){
+        // given
+        String providerName = "kakao";
+        String providerId = "11223344";
+        String nickname = "aaaa";
+
+        Member member = Member.builder()
+                .providerName(providerName)
+                .providerId(providerId)
+                .nickname(nickname)
+                .build();
+
+        memberRepository.save(member);
+
+        // when
+        Optional<Member> findMember = memberRepository.findByNickname(nickname);
+
+        // then
+        assertThat(findMember).isPresent();
     }
 }
