adb9cc2ab382570bacc80761783fa13c7b67ee8a
ktk8916 <ktk8916@naver.com>
2023-10-09 12:57:21 +0900

    질문 생성

--- src/main/java/malohaja/speak/interview/question/domain/request/QuestionCreateRequest.java
+++ src/main/java/malohaja/speak/interview/question/domain/request/QuestionCreateRequest.java
@@ -1,9 +1,5 @@
 package malohaja.speak.interview.question.domain.request;
 
-import malohaja.speak.global.domain.skill.SkillType;
-import malohaja.speak.interview.question.domain.entity.Question;
-import malohaja.speak.member.domain.entity.Member;
-
 import java.util.List;
 
 public record QuestionCreateRequest(
@@ -11,11 +7,4 @@ public record QuestionCreateRequest(
         List<String> skills
 ) {
 
-    public Question toEntity(Long memberId){
-        return Question.builder()
-                .content(content)
-                .member(Member.fromId(memberId))
-                .skills(SkillType.convertList(skills))
-                .build();
-    }
 }
--- src/main/java/malohaja/speak/interview/question/service/QuestionCommandService.java
+++ src/main/java/malohaja/speak/interview/question/service/QuestionCommandService.java
@@ -4,15 +4,20 @@ import lombok.RequiredArgsConstructor;
 import malohaja.speak.global.domain.skill.SkillType;
 import malohaja.speak.global.jwt.TokenInfo;
 import malohaja.speak.interview.question.domain.entity.Question;
+import malohaja.speak.interview.question.domain.entity.QuestionSkill;
 import malohaja.speak.interview.question.domain.request.QuestionCreateRequest;
 import malohaja.speak.interview.question.domain.request.QuestionUpdateRequest;
 import malohaja.speak.interview.question.domain.response.QuestionResponse;
 import malohaja.speak.interview.question.exception.InvalidQuestionWriterException;
 import malohaja.speak.interview.question.repository.QuestionRepository;
+import malohaja.speak.member.domain.entity.Member;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,11 +27,20 @@ public class QuestionCommandService {
     private final QuestionRepository questionRepository;
 
     public QuestionResponse questionCreate(TokenInfo tokenInfo, QuestionCreateRequest request){
-        Question question = request.toEntity(tokenInfo.getId());
+        Question question = Question.builder()
+                .member(Member.fromId(tokenInfo.getId()))
+                .content(request.content())
+                .build();
+
         Question savedQuestion = questionRepository.save(question);
+
+        Set<QuestionSkill> skills = of(question, request.skills());
+
+        savedQuestion.setSkills(skills);
 
         return QuestionResponse.fromEntity(savedQuestion);
     }
+
     public QuestionResponse questionUpdate(Long id, TokenInfo tokenInfo, QuestionUpdateRequest request){
         Question question = questionRepository.getByQuestionId(id);
 
@@ -34,8 +48,20 @@ public class QuestionCommandService {
             throw new InvalidQuestionWriterException();
         }
 
-        question.update(request.content(), SkillType.convertList(request.skills()));
+        Set<QuestionSkill> skills = of(question, request.skills());
+
+        question.update(request.content(), skills);
+
         return QuestionResponse.fromEntity(question);
+    }
+
+    private static Set<QuestionSkill> of(Question question, List<String> skills) {
+        return SkillType.convertList(skills).stream()
+                .map(skill -> QuestionSkill.builder()
+                        .question(question)
+                        .skill(skill)
+                        .build())
+                .collect(Collectors.toSet());
     }
 
     private boolean isValidMember(Question question, Long memberId){
