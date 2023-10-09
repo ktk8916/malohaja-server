8862afd8ae63e7f2043ec99632a6ab1e52905708
ktk8916 <ktk8916@naver.com>
2023-10-09 22:54:00 +0900

    북마크 추가, 제거

--- src/main/java/malohaja/speak/interview/question/controller/QuestionCommandController.java
+++ src/main/java/malohaja/speak/interview/question/controller/QuestionCommandController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import malohaja.speak.global.jwt.TokenInfo;
 import malohaja.speak.interview.question.domain.request.QuestionCreateRequest;
 import malohaja.speak.interview.question.domain.request.QuestionUpdateRequest;
-import malohaja.speak.interview.question.domain.response.QuestionDetailResponse;
 import malohaja.speak.interview.question.domain.response.QuestionResponse;
 import malohaja.speak.interview.question.service.QuestionCommandService;
 import org.springframework.http.HttpStatus;
@@ -35,5 +34,41 @@ public class QuestionCommandController {
             @RequestBody QuestionUpdateRequest request
     ){
         return questionCommandService.questionUpdate(id, tokenInfo, request);
+    }
+
+    @PostMapping("/{id}/bookmark")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addBookmark(
+            @AuthenticationPrincipal TokenInfo tokenInfo,
+            @PathVariable("id") Long id
+    ){
+        questionCommandService.addBookmark(id, tokenInfo);
+    }
+
+    @DeleteMapping("/{id}/bookmark")
+        @ResponseStatus(HttpStatus.NO_CONTENT)
+        public void removeBookmark(
+                @AuthenticationPrincipal TokenInfo tokenInfo,
+                @PathVariable("id") Long id
+    ){
+        questionCommandService.removeBookmark(id, tokenInfo);
+    }
+
+    @PostMapping("/{id}/like")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addLike(
+            @AuthenticationPrincipal TokenInfo tokenInfo,
+            @PathVariable("id") Long id
+    ){
+        questionCommandService.addBookmark(id, tokenInfo);
+    }
+
+    @DeleteMapping("/{id}/like")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeLike(
+            @AuthenticationPrincipal TokenInfo tokenInfo,
+            @PathVariable("id") Long id
+    ){
+        questionCommandService.removeBookmark(id, tokenInfo);
     }
 }
