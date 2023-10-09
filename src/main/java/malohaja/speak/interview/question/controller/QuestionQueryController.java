2c3c8864986eeea1b17cc5d7d06cfbd6acea6d64
ktk8916 <ktk8916@naver.com>
2023-10-09 22:54:54 +0900

    내 북마크 조회

--- src/main/java/malohaja/speak/interview/question/controller/QuestionQueryController.java
+++ src/main/java/malohaja/speak/interview/question/controller/QuestionQueryController.java
@@ -1,11 +1,13 @@
 package malohaja.speak.interview.question.controller;
 
 import lombok.RequiredArgsConstructor;
+import malohaja.speak.global.jwt.TokenInfo;
 import malohaja.speak.interview.question.domain.request.QuestionSearchCondition;
 import malohaja.speak.interview.question.domain.response.QuestionCardResponse;
 import malohaja.speak.interview.question.domain.response.QuestionDetailResponse;
 import malohaja.speak.interview.question.service.QuestionQueryService;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,11 +26,16 @@ public class QuestionQueryController {
 
     @GetMapping
     public List<QuestionCardResponse> getByCondition(
+            QuestionSearchCondition condition,
             @RequestParam(value = "page", required = false, defaultValue = "0") int page,
-            @RequestParam(value = "size", required = false, defaultValue = "16") int size,
-            QuestionSearchCondition condition
+            @RequestParam(value = "size", required = false, defaultValue = "16") int size
     ){
         PageRequest pageRequest = PageRequest.of(page, size);
         return questionQueryService.getByCondition(condition, pageRequest);
+    }
+
+    @GetMapping("/bookmark/my")
+    public List<QuestionCardResponse> getMyBookmark(@AuthenticationPrincipal TokenInfo tokenInfo){
+        return questionQueryService.getMyBookmark(tokenInfo);
     }
 }
